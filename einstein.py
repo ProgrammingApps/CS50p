d10b2f455fc02320ff681a22029d1f58a48db8cc
Josh Paul Apps <JoshPaulApps@gmail.com>
2026-01-21 10:41:46 +0000

    Added einstein and faces

---
+++ einstein.py
@@ -0,0 +1,8 @@
+#E = MC**2
+
+
+c = 300000000
+mass = int(input("What's the mass of your object?"))
+
+E = mass*(c**2)
+print(str(E))
--- faces.py
+++ faces.py
@@ -1,7 +1,11 @@
 def main():
-    convert()
+    response = input("Hey, how're you doing today?")
+    convert(response)
 
 
-def convert():
-    
-    
+def convert(user_input):
+    converted_text = user_input.replace(":)", "🙂")
+    converted_text = converted_text.replace(":(", "🙁")
+    print(converted_text)
+
+main()
