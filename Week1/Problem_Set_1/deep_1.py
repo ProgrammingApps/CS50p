01a8b6444381898022bd886a3c7b0e53ec4da854
Josh Paul Apps <JoshPaulApps@gmail.com>
2026-01-26 11:29:20 +0000

    Added deep python files

---
+++ Week1/Problem_Set_1/deep_1.py
@@ -0,0 +1,7 @@
+answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything?")
+
+if answer == "Fourty-Two" or "Fourty Two" or "42":
+    print("Yes")
+else:
+    print("No")
+
