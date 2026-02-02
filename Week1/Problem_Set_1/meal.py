c99a50b17803d77bd9f27e7923ce4bdcc7b501df
Josh Paul Apps <JoshPaulApps@gmail.com>
2026-02-02 18:42:09 +0000

    Passed tests

--- Week1/Problem_Set_1/meal.py
+++ Week1/Problem_Set_1/meal.py
@@ -12,11 +12,11 @@ def main():
     converted_time = convert(time)
 
     if 7 <= converted_time <= 8:
-        print("Breakfast")
+        print("breakfast time")
     elif 12 <= converted_time <= 13:
-        print("Lunch")
-    elif 16 <= converted_time <= 17:
-        print("Dinner")
+        print("lunch time")
+    elif 18 <= converted_time <= 19:
+        print("dinner time")
 
 
 def convert(time):
