0a0b3dda7ea932fc1fe561b0fab7ba91a4794749
Josh Paul Apps <JoshPaulApps@gmail.com>
2026-02-21 20:06:21 +0000

    added hogwarts.py

--- Week2/hogwarts.py
+++ Week2/hogwarts.py
@@ -1,3 +1,34 @@
+"""
 students = ["hermione", "harry", "ron"]
 
-print(students[0])
+#for student in students:
+   # print(student)
+
+for number in range(len(students)):
+    print(number + 1, students[number])
+
+    
+
+students = ["Hermione", "Harry", "Ron", "Draco"]
+houses = ["Gryffindor", "Gryffindor", "Gryffindor", "Slytherin"]
+
+
+
+
+students = {"Hermione": "Gryffindor",
+            "Harry": "Gryffindor",
+            "Ron": "Gryffindor",
+            "Draco": "Slytherin"} 
+
+for student in students:
+    print(student, students[student], sep = ", ")
+
+    """
+
+students = [
+    {"name" : "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russel Terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None}
+]
+
