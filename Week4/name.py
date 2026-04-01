4ebca2070e32fc72295e38cf01013f8f2a55e728
Josh Paul Apps <JoshPaulApps@gmail.com>
2026-04-01 16:04:44 +0100

    Completed system print

--- Week4/name.py
+++ Week4/name.py
@@ -2,7 +2,6 @@ import sys
 
 if len(sys.argv) <= 1:
     sys.exit("Too few values")
-elif len(sys.argv) > 2:
-    sys.exit("Too many values")
 
-print("Hello, my name is", sys.argv[1])
+for arg in sys.argv[1:]:
+    print(f"Hello, my name is {arg}")
