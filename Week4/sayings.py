b53320cad125ba25a5379ddf60cf76b9e0168104
Josh Paul Apps <JoshPaulApps@gmail.com>
2026-04-06 17:47:08 +0100

    sayings added

---
+++ Week4/sayings.py
@@ -0,0 +1,12 @@
+def hello(name):
+    print(f"Hello, {name}")
+
+def goodbye(name):
+    print(f"Goodbye, {name}")
+
+def main():
+    hello("world")
+    goodbye("world")
+
+if __name__ == "__main__":
+    main()
