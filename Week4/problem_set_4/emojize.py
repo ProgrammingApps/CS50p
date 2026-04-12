4d2791b297a916361e0f221d66ecf863aac865e0
Josh Paul Apps <JoshPaulApps@gmail.com>
2026-04-12 13:05:36 +0100

    created emoji file

--- Week4/problem_set_4/emojize.py
+++ Week4/problem_set_4/emojize.py
@@ -1,8 +1,14 @@
 #In a file called emojize.py, implement a program that prompts the user 
 # for a str in English and then outputs the “emojized” version of that str,
 #converting any codes (or aliases) therein to their corresponding emoji.
+import emoji
 
 def main():
-    ...
+    emoji_creator()
+
+def emoji_creator():
+    response = input("Input: ")
+    print(emoji.emojize(response))
+
 
 main()
